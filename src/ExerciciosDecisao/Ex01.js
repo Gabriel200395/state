a08e0d7a9dadf565a70f2dc518288a1ca7569b3f
Gabriel Souza <eugabrielf@gmail.com>
2021-03-27 12:36:08 -0300

    resolvendo o primeiro exercicio

--- src/ExerciciosDecisao/Ex01.js
+++ src/ExerciciosDecisao/Ex01.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 function Ex01() {
-  return <div />;
+  const [numero1, setNumero1] = useState("");
+  const [numero2, setNumero2] = useState("");
+  const [resultado, setResultado] = useState(0);
+
+  function vericarMaior() {
+    (+numero1 > +numero2 && setResultado(numero1)) ||
+      (+numero2 > +numero1 && setResultado(numero2));
+  }
+
+  return (
+    <div>
+      {resultado}
+      <input
+        type="number"
+        value={numero1}
+        onChange={({ target }) => setNumero1(target.value)}
+      />
+      <input
+        type="number"
+        value={numero2}
+        onChange={({ target }) => setNumero2(target.value)}
+      />
+      <button onClick={vericarMaior}>verificar maior numero</button>
+    </div>
+  );
 }
 
 export default Ex01;
